6848c914e01bb3fd5343a417c020aacc62abe836
Michael Kaiser-Cross <mkaisercross@gmail.com>
2015-06-14 23:09:22 -0400

    forgot to update var name

--- src/android/ReceiptOcr.java
+++ src/android/ReceiptOcr.java
@@ -32,7 +32,7 @@ public class ReceiptOcr extends CordovaPlugin {
         if (action.equals("run")) {
             String imageJpegBase64 = args.getString(0);
             //ByteBuffer imageBuffer = ByteBuffer.wrap();
-            byte[] imageJpeg = Base64.decode(imageStringBase64, Base64.DEFAULT);
+            byte[] imageJpeg = Base64.decode(imageJpegBase64, Base64.DEFAULT);
             Bitmap image = decodeByteArray(imageJpeg, 0, imageJpeg.length, ARGB_8888);
             //Bitmap image = Bitmap.createBitmap(1024,768);
             //image.copyPixelsFromBuffer(imageBuffer);
